6b449b30c511c3edb661f01f8c7fc98337386290
Serge Semenov <tyrotoxin@users.noreply.github.com>
2017-12-01 14:25:54 -0800

    Fix InfoBlock flow in Execution Context for .NET 4.5 - attempt number 2 (#7)
    * Fix InfoBlock flow in Execution Context for .NET 4.5

--- src/Contextually.NET45/AsyncLocal.cs
+++ src/Contextually.NET45/AsyncLocal.cs
@@ -1,53 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
 using System.Runtime.Remoting.Messaging;
+using Contextually.Reflection;
 
 namespace System.Threading
 {
-    internal static class AsyncLocal
-    {
-        private const string ContextPropertyNamePrefix = "ctxal"; // short from "Contextually Async Local"
-
-        private static int _nameCounter;
-
-        public static string AllocateName()
-        {
-            var index = Interlocked.Increment(ref _nameCounter);
-            return ContextPropertyNamePrefix + index;
-        }
-    }
-
     /// <summary>
     /// Polyfill for AsyncLocal functionality added in NETSTANDARD1.3 and .NET 4.6
     /// </summary>
     /// <typeparam name="T">
     /// The type of the value to encapsulate and follow.
     /// </typeparam>
-    internal sealed class AsyncLocal<T>
+    internal class AsyncLocal<T>
     {
-        private static readonly Dictionary<int, T> _valueMap = new Dictionary<int, T>();
-        private static readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
-        private static int _valueCounter;
-
-        private readonly string _name = AsyncLocal.AllocateName();
-
-        private int? CurrentValueId
-        {
-            get
-            {
-                return (int?)CallContext.LogicalGetData(_name);
-            }
-            set
-            {
-                if (value.HasValue)
-                {
-                    CallContext.LogicalSetData(_name, value.Value);
-                }
-                else
-                {
-                    CallContext.FreeNamedDataSlot(_name);
-                }
-            }
-        }
+        private readonly string _name = Guid.NewGuid().ToString("N");
 
         /// <summary>
         /// Gets or set the value to flow with <see cref="ExecutionContext"/>.
@@ -56,62 +21,19 @@ namespace System.Threading
         {
             get
             {
-                object value = null;
-
-                int? id = CurrentValueId;
-                if (id.HasValue)
-                {
-                    _lock.EnterReadLock();
-                    try
-                    {
-                        value = _valueMap[id.Value];
-                    }
-                    finally
-                    {
-                        _lock.ExitReadLock();
-                    }
-                }
-
-                return value == null ? default(T) : (T)value;
+                return (T)CallContext.LogicalGetData(_name);
             }
             set
             {
-                int? id = CurrentValueId;
-                if (value == null)
-                {
-                    if (id.HasValue)
-                    {
-                        _lock.EnterWriteLock();
-                        try
-                        {
-                            _valueMap.Remove(id.Value);
-                        }
-                        finally
-                        {
-                            _lock.ExitWriteLock();
-                        }
-                    }
-                    CurrentValueId = null;
-                }
-                else
-                {
-                    if (!id.HasValue)
-                    {
-                        id = Interlocked.Increment(ref _valueCounter);
-                        CurrentValueId = id;
-                    }
-
-                    _lock.EnterWriteLock();
-                    try
-                    {
-                        _valueMap[id.Value] = value;
-                    }
-                    finally
-                    {
-                        _lock.ExitWriteLock();
-                    }
-                }
+                // Mimic the implementation of AsyncLocal<T>
+                var executionContext = Thread.CurrentThread.GetMutableExecutionContext();
+                var logicalCallContext = executionContext.GetLogicalCallContext();
+                var datastore = logicalCallContext.GetDatastore();
+                var datastoreCopy = datastore == null ? new Hashtable() : new Hashtable(datastore);
+                datastoreCopy[_name] = value;
+                logicalCallContext.SetDatastore(datastoreCopy);
             }
         }
+
     }
 }
---
+++ src/Contextually.NET45/Reflection/ExecutionContextExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+
+namespace Contextually.Reflection
+{
+    internal static class ExecutionContextExtensions
+    {
+        private static readonly Func<ExecutionContext, LogicalCallContext> _getLogicalCallContextFunc;
+
+        static ExecutionContextExtensions()
+        {
+            var logicalCallContextPropertyInfo = typeof(ExecutionContext).GetProperty("LogicalCallContext", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            var instanceParameterExpression = Expression.Parameter(typeof(ExecutionContext));
+            var memberAccessExpression = Expression.MakeMemberAccess(instanceParameterExpression, logicalCallContextPropertyInfo);
+            var lambdaExpression = Expression.Lambda<Func<ExecutionContext, LogicalCallContext>>(memberAccessExpression, instanceParameterExpression);
+            _getLogicalCallContextFunc = lambdaExpression.Compile();
+        }
+
+        public static LogicalCallContext GetLogicalCallContext(this ExecutionContext executionContext)
+            => _getLogicalCallContextFunc(executionContext);
+    }
+}
