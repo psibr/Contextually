8aac0e5f5fd6cdd0182c51304045155ad2a49214
Ovan Crone <ovan@psibr.net>
2017-12-18 23:03:49 -0800

    :bug: :x: Add failing test showing AppDomain issue

---
+++ test/Contextually.Tests.Net45/AppDomainTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Contextually.Tests.Net45
+{
+    [TestClass]
+    [TestCategory("AppDomain")]
+    public class AppDomainTests
+    {
+        [TestMethod]
+        public void InfoBlockTraversesAppDomainsCorrectly()
+        {
+            NameValueCollection blockValues;
+            var key = "SomeKey";
+            var value = "SomeValue";
+
+            using (Relevant.Info(new NameValueCollection { { key, value } }))
+            {
+                var tempDomain = AppDomain.CreateDomain("tempDomain", 
+                    AppDomain.CurrentDomain.Evidence,
+                    AppDomain.CurrentDomain.BaseDirectory, 
+                    AppDomain.CurrentDomain.RelativeSearchPath,
+                    AppDomain.CurrentDomain.ShadowCopyFiles);
+
+                blockValues = Relevant.Info();
+            }
+
+            Assert.IsNotNull(blockValues);
+            Assert.AreEqual(1, blockValues.Count);
+            Assert.AreEqual(key, blockValues.GetKey(0));
+
+            var values = blockValues.GetValues(key);
+            Assert.IsNotNull(values);
+            Assert.AreEqual(1, values);
+            Assert.AreEqual(value, values[0]);
+        }
+
+    }
+}
