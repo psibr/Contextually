4e375efe8cfaab9d1494bf0881da52cd549f0796
Sergii Semenov <sergiis@live.com>
2018-06-15 21:38:42 -0700

    Add exception extensions

---
+++ src/Contextually/ExceptionExtensions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace Contextually
+{
+    /// <summary>
+    /// Extension methods for the <see cref="Exception"/> type.
+    /// </summary>
+    public static class ExceptionExtensions
+    {
+        /// <summary>
+        /// Copies the <see cref="Relevant.Info()"/> to the <see cref="Exception.Data"/>
+        /// and returns the same instance of an exception.
+        /// You can retrieve it back with <see cref="GetReleventInfo(Exception)"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of an exception.</typeparam>
+        /// <param name="exception">The instance of an exception to attach the relevant info to.</param>
+        /// <param name="containerName">Optional name of the container to read from.</param>
+        /// <returns>Simply returns the <paramref name="exception"/>.</returns>
+        public static T WithRelevantInfo<T>(this T exception, string containerName = null)
+            where T : Exception
+        {
+            var info = Relevant.Info(containerName);
+            foreach (string name in info.Keys)
+                exception.Data[name] = info[name];
+            return exception;
+        }
+
+        /// <summary>
+        /// Copies the <see cref="Relevant.Info()"/> to the <see cref="Exception.Data"/>.
+        /// You can retrieve it back with <see cref="GetReleventInfo(Exception)"/>.
+        /// </summary>
+        /// <param name="exception">The instance of an exception to attach the relevant info to.</param>
+        /// <param name="containerName">Optional name of the container to read from.</param>
+        public static void AttachRelevantInfo(this Exception exception, string containerName = null)
+            => exception.WithRelevantInfo(containerName);
+
+        /// <summary>
+        /// Collects revelant info previously added with <see cref="AttachRelevantInfo(Exception, string)"/>
+        /// or <see cref="WithRelevantInfo{T}(T, string)"/>, including inner exceptions.
+        /// </summary>
+        /// <param name="exception">The exception root to get relevant info from.</param>
+        public static NameValueCollection GetReleventInfo(this Exception exception)
+        {
+            var relevantInfo = new NameValueCollection();
+            PopulateReleventInfo(exception, relevantInfo);
+            return relevantInfo;
+
+            void PopulateReleventInfo(Exception ex, NameValueCollection info)
+            {
+                foreach (var key in ex.Data.Keys)
+                {
+                    // Add name-value pair to the info collection only if:
+                    // 1. Both name and value are strings
+                    // 2. the info collection does not contain the given name already -
+                    //    assume that wraping exception overrides values of inner exceptions.
+                    if (key is string name && ex.Data[key] is string value && info[name] == null)
+                    {
+                        info.Add(name, value);
+                    }
+                }
+
+                // Gather relevant info recursively.
+                if (ex is AggregateException aggregateException)
+                {
+                    foreach (var innerException in aggregateException.InnerExceptions)
+                    {
+                        PopulateReleventInfo(innerException, info);
+                    }
+                }
+                else if (ex.InnerException != null)
+                {
+                    PopulateReleventInfo(ex.InnerException, info);
+                }
+            }
+        }
+    }
+}
---
+++ test/Contextually.Tests/ExceptionTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Contextually.Tests
+{
+    [TestClass]
+    [TestCategory("Asynchronyous")]
+    public class ExceptionTests
+    {
+        [TestMethod]
+        public async Task ExceptionConveysRelevantInfo()
+        {
+            // ARRANGE
+            var exception = new Exception();
+
+            // ACT
+            using (Relevant.Info("key1", "value1"))
+            {
+                exception.AttachRelevantInfo();
+            }
+
+            var actualRelevantInfo = exception.GetReleventInfo();
+
+            // ASSERT
+            Assert.IsNotNull(actualRelevantInfo, "should return a value");
+            Assert.AreEqual(1, actualRelevantInfo.Count, $"should have 1 pair");
+            Assert.AreEqual("value1", actualRelevantInfo["key1"], "value must match");
+        }
+
+        [TestMethod]
+        public async Task InnerExceptionConveysRelevantInfo()
+        {
+            // ARRANGE
+            var innerException = new Exception("inner error");
+            var topException = new Exception("error", innerException);
+
+            // ACT
+            using (Relevant.Info("key1", "value1"))
+            {
+                innerException.AttachRelevantInfo();
+            }
+
+            using (Relevant.Info("key2", "value2"))
+            {
+                topException.AttachRelevantInfo();
+            }
+
+            var actualRelevantInfo = topException.GetReleventInfo();
+
+            // ASSERT
+            Assert.IsNotNull(actualRelevantInfo, "should return a value");
+            Assert.AreEqual(2, actualRelevantInfo.Count, $"should have 2 pairs");
+            Assert.AreEqual("value1", actualRelevantInfo["key1"], "value 1 must match");
+            Assert.AreEqual("value2", actualRelevantInfo["key2"], "value 2 must match");
+        }
+
+        [TestMethod]
+        public async Task TopExceptionOverridesRelevantInfo()
+        {
+            // ARRANGE
+            var innerException = new Exception("inner error");
+            var topException = new Exception("error", innerException);
+
+            // ACT
+            using (Relevant.Info("key", "value to be overriden"))
+            {
+                innerException.AttachRelevantInfo();
+            }
+
+            using (Relevant.Info("key", "value"))
+            {
+                topException.AttachRelevantInfo();
+            }
+
+            var actualRelevantInfo = topException.GetReleventInfo();
+
+            // ASSERT
+            Assert.IsNotNull(actualRelevantInfo, "should return a value");
+            Assert.AreEqual(1, actualRelevantInfo.Count, $"should have 1 pair");
+            Assert.AreEqual("value", actualRelevantInfo["key"], "value must match");
+        }
+
+        [TestMethod]
+        public async Task AggregateRelevantInfo()
+        {
+            // ARRANGE
+            var innerException1 = new Exception("inner error 1");
+            var innerException2 = new Exception("inner error 2");
+            var topException = new AggregateException(innerException1, innerException2);
+
+            // ACT
+            using (Relevant.Info("key1", "value1"))
+            {
+                innerException1.AttachRelevantInfo();
+            }
+
+            using (Relevant.Info("key2", "value2"))
+            {
+                innerException2.AttachRelevantInfo();
+            }
+
+            using (Relevant.Info("key3", "value3"))
+            {
+                topException.AttachRelevantInfo();
+            }
+
+            var actualRelevantInfo = topException.GetReleventInfo();
+
+            // ASSERT
+            Assert.IsNotNull(actualRelevantInfo, "should return a value");
+            Assert.AreEqual(3, actualRelevantInfo.Count, $"should have 3 pairs");
+            Assert.AreEqual("value1", actualRelevantInfo["key1"], "value 1 must match");
+            Assert.AreEqual("value2", actualRelevantInfo["key2"], "value 2 must match");
+            Assert.AreEqual("value3", actualRelevantInfo["key3"], "value 3 must match");
+        }
+    }
+}
